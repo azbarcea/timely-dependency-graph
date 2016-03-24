e72cd2039e7a26f6c6f4d50eba0131b4fa7a3ce3
mindrones <mindrones@mindrones.local>
2016-03-24 13:05:50 +0100

    twitter cards don't support animated gifs: adding d3_modules_d3_axis_sets.png instead

--- gulptasks/images.js
+++ gulptasks/images.js
@@ -2,7 +2,10 @@ const gulp = require('gulp');
 const gutil = require('gulp-util');
 
 gulp.task('images', function() {
-    return gulp.src(['./doc/images/usage_quick.gif'], {base: './doc/images/'})
+    return gulp.src([
+        './doc/images/usage_quick.gif',
+        './doc/images/d3_modules_d3_axis_sets.png'
+    ], {base: './doc/images/'})
     .pipe(gulp.dest('./build/dist'))
     .on('error', gutil.log)
     ;
