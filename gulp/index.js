2c8c899c4be7bc998494809d8d053b5444609828
mindrones <mindrones@users.noreply.github.com>
2016-05-19 16:59:51 +0200

    gulp: removing requireDir from dependencies
    (using `import` instead)

--- gulp/index.js
+++ gulp/index.js
@@ -1,2 +1,9 @@
-import { default as requireDir } from 'require-dir'
-requireDir('./tasks', {recurse: true});
+import './tasks/assets'
+import './tasks/build'
+import './tasks/data'
+import './tasks/default'
+import './tasks/html'
+import './tasks/images'
+import './tasks/logic'
+import './tasks/serve'
+import './tasks/style'
--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -1,3 +1,4 @@
 import { default as gulp } from 'gulp'
 import { default as runSequence } from 'run-sequence'
+
 gulp.task('default', () => { runSequence('build', 'serve') })
